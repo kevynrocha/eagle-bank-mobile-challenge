553db5262be5e0acbe9f70fc46b1e5b8255b5e14
Kevyn Rocha <kevyn_oliver@hotmail.com>
2021-01-02 17:52:41 -0300

    feat(input): add input component

---
+++ src/components/shared/Input/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef } from 'react';
+
+import { TextInput, TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
+
+import * as S from './styles';
+
+interface InputProps extends TextInputProps {
+  name: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, ...rest }) => {
+  const inputRef = useRef<TextInput & TextInputProps>(null);
+  const { fieldName, registerField, defaultValue = '', error } = useField(name);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = defaultValue;
+    }
+  }, [defaultValue]);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value'
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <>
+      <S.Input
+        ref={inputRef}
+        keyboardAppearance="dark"
+        defaultValue={defaultValue}
+        placeholderTextColor="#666360"
+        onChangeText={value => {
+          if (inputRef.current) {
+            inputRef.current.value = value;
+          }
+        }}
+        {...rest}
+      />
+      {error && <S.TextError>{error}</S.TextError>}
+    </>
+  );
+};
+
+export default Input;
---
+++ src/components/shared/Input/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Input = styled.TextInput`
+  width: 100%;
+  padding: 10px;
+  border-color: #003573;
+  border-width: 1px;
+  margin-top: 20px;
+  margin-bottom: 5px;
+  border-radius: 4px;
+`;
+
+export const TextError = styled.Text`
+  font-size: 12px;
+  color: #962f2a;
+`;
