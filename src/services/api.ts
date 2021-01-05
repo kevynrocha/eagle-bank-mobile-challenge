2b97289d031a3541c04b10b8db3258c655502203
Kevyn Rocha <kevyn_oliver@hotmail.com>
2021-01-05 00:05:03 -0300

    refactor(serivces-api): change url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.100.20:3333/api/v1/',
+  baseURL: 'http://localhost:3333/api/v1/',
   validateStatus: status => {
     return status >= 200 && status <= 422;
   }
